5cfa089dca73ede38d549fa5bcb854552af4b1bc
ccie7599 <bapley@akamai.com>
2025-06-23 12:30:55 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,18 +1,29 @@
 variable "linode_token" {
   type        = string
-  description = "Linode API Token"
+  description = "Your Linode API Token"
 }
 
 variable "root_pass" {
   type        = string
-  description = "Root password for VMs"
+  description = "Root password for Linodes"
   sensitive   = true
 }
 
+variable "public_key_path" {
+  description = "Path to your SSH public key"
+  default     = "~/.ssh/id_rsa.pub"
+}
+
+variable "private_key_path" {
+  description = "Path to your SSH private key"
+  default     = "~/.ssh/id_rsa"
+}
+
 variable "region" {
-  default = "us-ord"
+  default = "us-east"
 }
 
 variable "varnish_nodes" {
   default = 3
 }
+
